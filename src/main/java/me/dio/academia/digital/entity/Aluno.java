a8805525a8beb2f7d48a7a89740ad2db78862372
Jeduneto <jeduneto@hotmail.com>
2022-05-05 17:55:30 -0300

    Versão final Sistema de Academia

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -1,24 +1,15 @@
 package me.dio.academia.digital.entity;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -28,21 +19,21 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Aluno {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	private String nome;
+  private String nome;
 
-	@Column(unique = true)
-	private String cpf;
+  @Column(unique = true)
+  private String cpf;
 
-	private String bairro;
-	
-	private LocalDate dataDeNascimento;
-	
-	@OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
-	@JsonIgnore
-	private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
